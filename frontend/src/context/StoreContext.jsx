f6811f333ddce41f55b810a5aa89f43edb17c7be
Harshit Singh <harshitsinghprof@gmail.com>
2025-05-18 23:39:53 +0530

    cart page done

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -20,10 +20,9 @@ const StoreContextProvider = (props)=>{
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
     }
 
-    useEffect(()=>{
-            console.log(cartItems);
-            
-    },[cartItems])
+    const getTotalCartAmount = ()=>{
+        
+    }
 
     const contextValue ={
         food_list,
